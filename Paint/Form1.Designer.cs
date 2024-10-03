eaeb1f18fb222e063716ca5541c607eebceab241
Artyom <shadow20051@mail.ru>
2024-10-03 19:21:49 +0300

    оно само нацальник

--- Paint/Form1.Designer.cs
+++ Paint/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            panel1 = new Panel();
             colorDialog1 = new ColorDialog();
             menuStrip1 = new MenuStrip();
             файлToolStripMenuItem = new ToolStripMenuItem();
@@ -43,16 +42,10 @@
             кругToolStripMenuItem = new ToolStripMenuItem();
             ластикToolStripMenuItem = new ToolStripMenuItem();
             палитраToolStripMenuItem = new ToolStripMenuItem();
+            pictureBox1 = new PictureBox();
             menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
-            // 
-            // panel1
-            // 
-            panel1.BackColor = SystemColors.ActiveCaptionText;
-            panel1.Location = new Point(11, 31);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(776, 401);
-            panel1.TabIndex = 0;
             // 
             // menuStrip1
             // 
@@ -60,8 +53,8 @@
             menuStrip1.Items.AddRange(new ToolStripItem[] { файлToolStripMenuItem, инструментыToolStripMenuItem, палитраToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Padding = new Padding(6, 3, 0, 3);
-            menuStrip1.Size = new Size(800, 30);
+            menuStrip1.Padding = new Padding(5, 2, 0, 2);
+            menuStrip1.Size = new Size(695, 24);
             menuStrip1.TabIndex = 7;
             menuStrip1.Text = "menuStrip1";
             // 
@@ -69,97 +62,115 @@
             // 
             файлToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { импортироватьToolStripMenuItem, сохранитьToolStripMenuItem, назадToolStripMenuItem, впередToolStripMenuItem });
             файлToolStripMenuItem.Name = "файлToolStripMenuItem";
-            файлToolStripMenuItem.Size = new Size(59, 24);
+            файлToolStripMenuItem.Size = new Size(48, 20);
             файлToolStripMenuItem.Text = "Файл";
             // 
             // импортироватьToolStripMenuItem
             // 
             импортироватьToolStripMenuItem.Name = "импортироватьToolStripMenuItem";
-            импортироватьToolStripMenuItem.Size = new Size(166, 26);
+            импортироватьToolStripMenuItem.Size = new Size(133, 22);
             импортироватьToolStripMenuItem.Text = "Открыть";
             // 
             // сохранитьToolStripMenuItem
             // 
             сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
-            сохранитьToolStripMenuItem.Size = new Size(166, 26);
+            сохранитьToolStripMenuItem.Size = new Size(133, 22);
             сохранитьToolStripMenuItem.Text = "Сохранить";
             // 
             // назадToolStripMenuItem
             // 
             назадToolStripMenuItem.Name = "назадToolStripMenuItem";
-            назадToolStripMenuItem.Size = new Size(166, 26);
+            назадToolStripMenuItem.Size = new Size(133, 22);
             назадToolStripMenuItem.Text = "Назад";
             // 
             // впередToolStripMenuItem
             // 
             впередToolStripMenuItem.Name = "впередToolStripMenuItem";
-            впередToolStripMenuItem.Size = new Size(166, 26);
+            впередToolStripMenuItem.Size = new Size(133, 22);
             впередToolStripMenuItem.Text = "Вперед";
             // 
             // инструментыToolStripMenuItem
             // 
             инструментыToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { кистьToolStripMenuItem, прямаяToolStripMenuItem, прямоугольникToolStripMenuItem, кругToolStripMenuItem, ластикToolStripMenuItem });
             инструментыToolStripMenuItem.Name = "инструментыToolStripMenuItem";
-            инструментыToolStripMenuItem.Size = new Size(117, 24);
+            инструментыToolStripMenuItem.Size = new Size(95, 20);
             инструментыToolStripMenuItem.Text = "Инструменты";
             // 
             // кистьToolStripMenuItem
             // 
             кистьToolStripMenuItem.Name = "кистьToolStripMenuItem";
-            кистьToolStripMenuItem.Size = new Size(224, 26);
+            кистьToolStripMenuItem.Size = new Size(180, 22);
             кистьToolStripMenuItem.Text = "Кисть";
+            кистьToolStripMenuItem.Click += кистьToolStripMenuItem_Click;
             // 
             // прямаяToolStripMenuItem
             // 
             прямаяToolStripMenuItem.Name = "прямаяToolStripMenuItem";
-            прямаяToolStripMenuItem.Size = new Size(224, 26);
+            прямаяToolStripMenuItem.Size = new Size(180, 22);
             прямаяToolStripMenuItem.Text = "Прямая";
+            прямаяToolStripMenuItem.Click += прямаяToolStripMenuItem_Click;
             // 
             // прямоугольникToolStripMenuItem
             // 
             прямоугольникToolStripMenuItem.Name = "прямоугольникToolStripMenuItem";
-            прямоугольникToolStripMenuItem.Size = new Size(224, 26);
+            прямоугольникToolStripMenuItem.Size = new Size(180, 22);
             прямоугольникToolStripMenuItem.Text = "Прямоугольник";
             прямоугольникToolStripMenuItem.Click += прямоугольникToolStripMenuItem_Click;
             // 
             // кругToolStripMenuItem
             // 
             кругToolStripMenuItem.Name = "кругToolStripMenuItem";
-            кругToolStripMenuItem.Size = new Size(224, 26);
+            кругToolStripMenuItem.Size = new Size(180, 22);
             кругToolStripMenuItem.Text = "Круг";
+            кругToolStripMenuItem.Click += кругToolStripMenuItem_Click;
             // 
             // ластикToolStripMenuItem
             // 
             ластикToolStripMenuItem.Name = "ластикToolStripMenuItem";
-            ластикToolStripMenuItem.Size = new Size(224, 26);
+            ластикToolStripMenuItem.Size = new Size(180, 22);
             ластикToolStripMenuItem.Text = "Ластик";
+            ластикToolStripMenuItem.Click += ластикToolStripMenuItem_Click;
             // 
             // палитраToolStripMenuItem
             // 
             палитраToolStripMenuItem.Name = "палитраToolStripMenuItem";
-            палитраToolStripMenuItem.Size = new Size(82, 24);
+            палитраToolStripMenuItem.Size = new Size(66, 20);
             палитраToolStripMenuItem.Text = "Палитра";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.White;
+            pictureBox1.Location = new Point(12, 27);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(671, 291);
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            pictureBox1.MouseDown += pictureBox1_MouseDown;
+            pictureBox1.MouseMove += pictureBox1_MouseMove;
+            pictureBox1.MouseUp += pictureBox1_MouseUp;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 451);
-            Controls.Add(panel1);
+            BackColor = SystemColors.AppWorkspace;
+            ClientSize = new Size(695, 330);
+            Controls.Add(pictureBox1);
             Controls.Add(menuStrip1);
             MainMenuStrip = menuStrip1;
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
+            Resize += Form1_Resize;
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private Panel panel1;
         private ColorDialog colorDialog1;
         private MenuStrip menuStrip1;
         private ToolStripMenuItem файлToolStripMenuItem;
@@ -174,5 +185,6 @@
         private ToolStripMenuItem кругToolStripMenuItem;
         private ToolStripMenuItem ластикToolStripMenuItem;
         private ToolStripMenuItem палитраToolStripMenuItem;
+        private PictureBox pictureBox1;
     }
 }
--- Paint/Form1.cs
+++ Paint/Form1.cs
@@ -4,18 +4,124 @@ namespace Paint
 {
     public partial class Form1 : Form
     {
+
+        public Bitmap bitfield;
+        Graphics gr;
+        delegate void Ddraw(int x, int y);
         public Form1()
         {
             InitializeComponent();
+            bitfield = new Bitmap(4096, 2160);
+            gr = Graphics.FromImage(bitfield);
+            my_draw = Ellipse;
         }
 
-        delegate void tool(Point x, int Size, Color color);
+        Ddraw my_draw;
+
+        bool flag = false;
+
+        void Ellipse(int x, int y)
+        {
+            //Graphics gr = CreateGraphics();
+            gr.DrawEllipse(Pens.Gray, x, y, 50, 50);
+
+        }
+
+        void Curve(int x, int y)
+        {
+            gr.DrawRectangle(Pens.Gray, x, y, 1, 1);
+        }
+
+        void Eraser(int x, int y)
+        {
+            gr.DrawRectangle(Pens.White, x, y, 1, 1);
+        }
+
+        void Line(int x, int y, int xs, int ys)
+        {
+            gr.DrawLine(Pens.Gray, x, y, xs, ys);
+        }
+
+        private Rectangle pictureBox1OriginalRectangle;
+        private Rectangle originalFormSize;
+
         private void Form1_Load(object sender, EventArgs e)
+        {
+            originalFormSize = new Rectangle(this.Location.X, this.Location.Y, this.Width, this.Height);
+            pictureBox1OriginalRectangle = new Rectangle(pictureBox1.Location.X, pictureBox1.Location.Y, pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void resizeControl(Rectangle r, Control c)
+        {
+            float xRatio = (float)this.Width / (float)(originalFormSize.Width);
+            float yRatio = (float)this.Height / (float)(originalFormSize.Height);
+
+            int newX = (int)(r.Location.X * xRatio);
+            int newY = (int)(r.Location.Y * yRatio);
+
+            int newWidth = (int)(r.Width * xRatio);
+            int newHeight = (int)(r.Height * yRatio);
+
+            c.Location = new Point(newX, newY);
+            c.Size = new Size(newWidth, newHeight);
+        }
+
+        private void ïðÿìîóãîëüíèêToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void ïðÿìîóãîëüíèêToolStripMenuItem_Click(object sender, EventArgs e)
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (my_draw == Curve || my_draw == Eraser)
+            { flag = true; }
+            Bitmap copy = bitfield.Clone(new Rectangle(0, 0, bitfield.Width, bitfield.Height), bitfield.PixelFormat);
+            my_draw(e.X, e.Y);
+            pictureBox1.Image = bitfield;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            resizeControl(pictureBox1OriginalRectangle, pictureBox1);
+        }
+
+        private void êðóãToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            my_draw = Ellipse;
+        }
+
+        private void êèñòüToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            my_draw = Curve;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (flag == true)
+            {
+                Bitmap copy = bitfield.Clone(new Rectangle(0, 0, bitfield.Width, bitfield.Height), bitfield.PixelFormat);
+                my_draw(e.X, e.Y);
+                pictureBox1.Image = bitfield;
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (flag == true)
+            {
+                Bitmap copy = bitfield.Clone(new Rectangle(0, 0, bitfield.Width, bitfield.Height), bitfield.PixelFormat);
+                my_draw(e.X, e.Y);
+                pictureBox1.Image = bitfield;
+                flag = false;
+            }
+        }
+
+        private void ëàñòèêToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            my_draw = Eraser;
+        }
+
+        private void ïðÿìàÿToolStripMenuItem_Click(object sender, EventArgs e)
         {
             
         }
